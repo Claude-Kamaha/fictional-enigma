d0bc62120dcf1fc3006b6c2e136181dc8cd9f2f8
KOfosu <kingsleyofosu@yahoo.com>
2021-04-28 07:51:37 +0000

    feat: update admin service

--- src/app/core/admin/admin.service.ts
+++ src/app/core/admin/admin.service.ts
@@ -32,94 +32,96 @@ import {
     providedIn: 'root'
 })
 export class AdminService {
+    apiHost = `https://sandbox.nellys-coin.ejaraapis.xyz/admin`;
 
     constructor(private httpClient: HttpClient) { }
 
     // ** crypto related methods
     saveCryptoSellingPrice = (payload: ISaveCryptoSellingPrice): Observable<IGenericReponse> => {
-        return this.httpClient.post<IGenericReponse>(`/api/v1/admin/save-crypto-selling-price`, payload);
+        return this.httpClient.post<IGenericReponse>(`${this.apiHost}/save-crypto-selling-price`, payload);
     }
 
     changeCryptoSettings = (payload: IChangeCryptoSettings): Observable<IGenericReponse> => {
-        return this.httpClient.put<IGenericReponse>(`/api/v1/admin/change-crypto-settings`, payload);
+        return this.httpClient.put<IGenericReponse>(`${this.apiHost}/change-crypto-settings`, payload);
     }
 
     updateCryptoBalance = (payload: IUpdateCryptoBalance): Observable<IGenericReponse> => {
-        return this.httpClient.put<IGenericReponse>(`/api/v1/admin/update-crypto-balance`, payload);
+        return this.httpClient.put<IGenericReponse>(`${this.apiHost}/update-crypto-balance`, payload);
     }
 
     updateCryptoUsdPrice = (payload: IUpdateCryptoUsdSellingPrice): Observable<IGenericReponse> => {
-        return this.httpClient.put<IGenericReponse>(`/api/v1/admin/update-crypto-usd-price`, payload);
+        return this.httpClient.put<IGenericReponse>(`${this.apiHost}/update-crypto-usd-price`, payload);
     }
 
 
     // ** customer and others related methods
     createCustomerFiatWallet = (payload: ICreateCustomerFiatWallet): Observable<ICreateCustomerFiatWalletResponse> => {
-        return this.httpClient.post<ICreateCustomerFiatWalletResponse>(`/api/v1/admin/create-fiat-wallet-for-customer`, payload);
+        return this.httpClient.post<ICreateCustomerFiatWalletResponse>(`${this.apiHost}/create-fiat-wallet-for-customer`, payload);
     }
 
     getCustomerListPerStatus = (params: HttpParams): Observable<IGetCustomerListPerStatusResponse> => {
-        return this.httpClient.get<IGetCustomerListPerStatusResponse>(`/api/v1/admin/get-customer-list-per-status`, { params });
+        return this.httpClient.get<IGetCustomerListPerStatusResponse>(`${this.apiHost}/get-customer-list-per-status`, { params });
     }
 
     getCustomerDetails = (params: HttpParams): Observable<IGetCustomerDetailsResponse> => {
-        return this.httpClient.get<IGetCustomerDetailsResponse>(`/api/v1/admin/get-customer-details`, { params });
+        return this.httpClient.get<IGetCustomerDetailsResponse>(`${this.apiHost}/get-customer-details`, { params });
     }
 
     getCustomerWallets = (params: HttpParams): Observable<IGetCustomerWalletsResponse> => {
-        return this.httpClient.get<IGetCustomerWalletsResponse>(`/api/v1/admin/get-customer-wallets`, { params });
+        return this.httpClient.get<IGetCustomerWalletsResponse>(`${this.apiHost}/get-customer-wallets`, { params });
     }
 
     updateCustomerLoyaltyLevel = (payload: IUpdateCustomerLoyaltyLevel): Observable<IGenericReponse> => {
-        return this.httpClient.put<IGenericReponse>(`/api/v1/admin/update-customer-loyalty-level`, payload);
+        return this.httpClient.put<IGenericReponse>(`${this.apiHost}/update-customer-loyalty-level`, payload);
     }
 
     changeCustomerType = (payload: IChangeCustomerType): Observable<IGenericReponse> => {
-        return this.httpClient.put<IGenericReponse>(`/api/v1/admin/change-customer-type`, payload);
+        return this.httpClient.put<IGenericReponse>(`${this.apiHost}/change-customer-type`, payload);
     }
 
     changeUserStatus = (payload: IChangeUserStatus): Observable<IGenericReponse> => {
-        return this.httpClient.put<IGenericReponse>(`/api/v1/admin/change-user-status`, payload);
+        return this.httpClient.put<IGenericReponse>(`${this.apiHost}/change-user-status`, payload);
     }
 
     updateCountryFeatureStatus = (payload: IUpdateCountryFeatureStatus): Observable<IGenericReponse> => {
-        return this.httpClient.put<IGenericReponse>(`/api/v1/admin/update-country-feature-status`, payload);
+        return this.httpClient.put<IGenericReponse>(`${this.apiHost}/update-country-feature-status`, payload);
     }
 
     updateCountryStatus = (payload: IUpdateCountryStatus): Observable<IGenericReponse> => {
-        return this.httpClient.put<IGenericReponse>(`/api/v1/admin/update-country-status`, payload);
+        return this.httpClient.put<IGenericReponse>(`${this.apiHost}/update-country-status`, payload);
     }
 
     migrateFiatWallet = (payload: IMigrateFiatWallet): Observable<IGenericReponse> => {
-        return this.httpClient.post<IGenericReponse>(`/api/v1/admin/migrate-fiat-wallet`, payload);
+        return this.httpClient.post<IGenericReponse>(`${this.apiHost}/migrate-fiat-wallet`, payload);
     }
+
 
     // ** transactions related methods
     saveMigrationData = (payload: ISaveMigrationData): Observable<IGenericReponse> => {
-        return this.httpClient.post<IGenericReponse>(`/api/v1/admin/save-migration-data`, payload);
+        return this.httpClient.post<IGenericReponse>(`${this.apiHost}/save-migration-data`, payload);
     }
 
     manualDeposit = (payload: IManualDeposit): Observable<IManualTransactionResponse> => {
-        return this.httpClient.post<IManualTransactionResponse>(`/api/v1/admin/manual-deposit`, payload);
+        return this.httpClient.post<IManualTransactionResponse>(`${this.apiHost}/manual-deposit`, payload);
     }
 
     manualBuy = (payload: IManualBuy): Observable<IManualTransactionResponse> => {
-        return this.httpClient.post<IManualTransactionResponse>(`/api/v1/admin/manual-buy`, payload);
+        return this.httpClient.post<IManualTransactionResponse>(`${this.apiHost}/manual-buy`, payload);
     }
 
     manualBuyPaymentUpdate = (payload: IManualBuyPaymentUpdate): Observable<IGenericReponse> => {
-        return this.httpClient.post<IGenericReponse>(`/api/v1/admin/manual-buy-payment-update`, payload);
+        return this.httpClient.post<IGenericReponse>(`${this.apiHost}/manual-buy-payment-update`, payload);
     }
 
     revertManualTransaction = (payload: IRevertManualTransaction): Observable<IGenericReponse> => {
-        return this.httpClient.post<IGenericReponse>(`/api/v1/admin/revert-manual-transaction`, payload);
+        return this.httpClient.post<IGenericReponse>(`${this.apiHost}/revert-manual-transaction`, payload);
     }
 
     changeMoonpaySettings = (payload: IChangeMoonpaySettings): Observable<IGenericReponse> => {
-        return this.httpClient.put<IGenericReponse>(`/api/v1/admin/change-moonpay-settings`, payload);
+        return this.httpClient.put<IGenericReponse>(`${this.apiHost}/change-moonpay-settings`, payload);
     }
 
     deleteTransaction = (params: HttpParams): Observable<IGenericReponse> => {
-        return this.httpClient.delete<IGenericReponse>(`/api/v1/admin/delete-transaction`, { params });
+        return this.httpClient.delete<IGenericReponse>(`${this.apiHost}/delete-transaction`, { params });
     }
 }
